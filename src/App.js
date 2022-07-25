dfde4f8f1383815c8a0b012549126bcf204e8f04
hmduong <hmduongwb14@gmail.com>
2022-07-25 12:08:07 +0700

    tét

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 //libaries
-import { Fragment } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { publicRoutes } from '~/routes';
-import { clsx } from 'clsx';
+import { Fragment } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { publicRoutes } from "~/routes";
+import { clsx } from "clsx";
 
-import { DefaultLayout } from './components/Layout';
+import { DefaultLayout } from "./components/Layout";
 
 function App() {
   return (
     <Router>
-      <div className={clsx('app')}>
+      <div className={clsx("app")}>
         <Routes>
           {publicRoutes.map((route, index) => {
             let Layout = DefaultLayout;
             if (route.layout) {
               Layout = route.layout;
             } else if (route.layout === null) {
+              console.log(123);
               Layout = Fragment;
             }
             const Page = route.page;
